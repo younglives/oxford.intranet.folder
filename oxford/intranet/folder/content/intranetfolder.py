2b9222ee27348fa2b043d8f161b4e71d4f8c2909
Michael Davis <m.r.davis@me.com>
2011-08-13 12:06:44 +0000

    Apply the placeful workflow

--- oxford/intranet/folder/content/intranetfolder.py
+++ oxford/intranet/folder/content/intranetfolder.py
@@ -4,6 +4,7 @@ from zope.interface import implements
 from plone.app.folder.folder import ATFolder
 
 from Products.Archetypes.atapi import registerType
+from Products.CMFCore.permissions import ModifyPortalContent
 
 from oxford.intranet.folder.config import PROJECTNAME
 from oxford.intranet.folder.interfaces.intranetfolder import IIntranetFolder
@@ -25,5 +26,21 @@ class IntranetFolder(ATFolder):
     security.declarePublic('canSetConstrainTypes')
     def canSetConstrainTypes(self):
         return False
+
+    security.declareProtected(ModifyPortalContent, 'applyLocalWorkflow')
+    def applyLocalWorkflow(self):
+        """
+        Apply a local workflow policy to this intranet folder.
+        """
+        placeful_workflow_tool = getToolByName(self, 'portal_placeful_workflow')
+        if WorkflowPolicyConfig_id not in self.objectIds():
+            new_policy_id = 'intranet'
+            policy = placeful_workflow_tool.getWorkflowPolicyById(new_policy_id)
+            if not policy:
+                return
+            wf_config = WorkflowPolicyConfig(new_policy_id, new_policy_id)
+            self._setObject(WorkflowPolicyConfig_id, wf_config)
+            workflow_tool = getToolByName(self, 'portal_workflow')
+            workflow_tool.updateRoleMappings()
 
 registerType(IntranetFolder, PROJECTNAME)

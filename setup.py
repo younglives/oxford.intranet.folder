eefc12e07a3923aac8484bcb9c418b54e534d655
Michael Davis <m.r.davis@me.com>
2015-01-02 22:39:45 +0000

    Readme in root folder

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version_path = os.path.join("oxford", "intranet", "folder", "version.txt")
 
 version = open(version_path).read().strip()
 
-long_description = open(os.path.join("docs", "README.txt")).read()
+long_description = open(os.path.join("README.txt")).read()
 long_description += "\n" + open(os.path.join("docs", "INSTALL.txt")).read()
 long_description += "\n" + open(os.path.join("docs", "HISTORY.txt")).read()
 
